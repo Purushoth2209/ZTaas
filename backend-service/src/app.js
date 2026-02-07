f03a9a560ad9e54e6eb94d059fb224fec0b1bf77
purushothaman.r <purushothaman.r@freightify.in>
2026-02-08 01:00:11 +0530

    added proper logging in backend-service

--- backend-service/src/app.js
+++ backend-service/src/app.js
@@ -4,14 +4,23 @@ import userRoutes from './routes/user.routes.js';
 import orderRoutes from './routes/order.routes.js';
 import jwksRoutes from './routes/jwks.routes.js';
 import { log } from './utils/logger.js';
+import { startTimer, getElapsedTime } from './utils/timer.js';
 
 const app = express();
 
 app.use(express.json());
 
 app.use((req, res, next) => {
-  const user = req.headers.authorization ? 'authenticated' : 'anonymous';
-  log(`${req.method} ${req.path} - ${user}`);
+  req.startTime = startTimer();
+  
+  const originalSend = res.send;
+  res.send = function(data) {
+    const elapsed = getElapsedTime(req.startTime);
+    const user = req.user ? `user=${req.user.username} role=${req.user.role}` : 'anonymous';
+    log(`method=${req.method} path=${req.path} ${user} status=${res.statusCode} latency=${elapsed}ms`);
+    return originalSend.call(this, data);
+  };
+  
   next();
 });
 
